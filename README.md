4c974ae5804e83be8c1cbe53fcead87c835970eb
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-03-01 18:01:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# conan-vk-bootstrap
+
+## Package Status
+
+| Windows | Linux | macOS |
+|:-------:|:-----:|:-----:|
+|[![Build status](https://ci.appveyor.com/api/projects/status/3yj7f0lg29uyuk04/branch/testing%2F0.2?svg=true)](https://ci.appveyor.com/project/SpaceIm/conan-vk-bootstrap)|[![Build status](https://github.com/SpaceIm/conan-vk-bootstrap/workflows/.github/workflows/conan.yml/badge.svg?branch=testing%2F0.2)](https://github.com/SpaceIm/conan-vk-bootstrap/actions?query=branch%3Atesting%2F0.2)|[![Build Status](https://travis-ci.com/SpaceIm/conan-vk-bootstrap.svg?branch=testing%2F0.2)](https://travis-ci.com/SpaceIm/conan-vk-bootstrap)|
+
+## Description
+
+[Conan.io](https://conan.io) package for [vk-bootstrap](https://github.com/charles-lunarg/vk-bootstrap).
---
+++ conanfile.py
@@ -0,0 +1,77 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+class VkBootstrapConan(ConanFile):
+    name = "vk-bootstrap"
+    description = "Vulkan bootstraping library."
+    license = "MIT"
+    topics = ("conan", "vk-bootstrap", "vulkan")
+    homepage = "https://github.com/charles-lunarg/vk-bootstrap"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, 11)
+        if self.settings.compiler == "Visual Studio" and self.options.shared:
+            raise ConanInvalidConfiguration("vk-boostrap shared not supported with Visual Studio")
+
+    def requirements(self):
+        self.requires("vulkan-headers/1.2.170.0")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename(self.name + "-" + self.version, self._source_subfolder)
+
+    def _patch_sources(self):
+        cmakelists = os.path.join(self._source_subfolder, "CMakeLists.txt")
+        # We don't need full Vulkan SDK, just headers, but vulkan-headers recipe alone can't emulate FindVulkan.cmake
+        tools.replace_in_file(cmakelists, "find_package(Vulkan REQUIRED)", "")
+        # No warnings as errors
+        tools.replace_in_file(cmakelists, "-pedantic-errors", "")
+        tools.replace_in_file(cmakelists, "/WX", "")
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["CMAKE_CXX_STANDARD"] = self.settings.compiler.get_safe("cppstd", 11)
+        self._cmake.definitions["VK_BOOTSTRAP_TEST"] = False
+        self._cmake.configure()
+        return self._cmake
+
+    def build(self):
+        self._patch_sources()
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "src"))
+        self.copy("*.lib", dst="lib", src="lib")
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.a", dst="lib", src="lib")
+        self.copy("*.so*", dst="lib", src="lib", symlinks=True)
+        self.copy("*.dylib", dst="lib", src="lib", symlinks=True)
+
+    def package_info(self):
+        self.cpp_info.libs = ["vk-bootstrap"]
